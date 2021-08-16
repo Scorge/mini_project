c096fa67b6fcad10d4366711c13723132eb202ed
Scorge <shindong1992@gmail.com>
2021-08-16 22:30:35 +0900

    몹 리젠 기능 추가
     - SpawnPoints 지정 및 지정 좌표에서 몹 생성 로직 추가

--- mini_game/Assets/Scripts/CameraChase.cs
+++ mini_game/Assets/Scripts/CameraChase.cs
@@ -11,16 +11,29 @@ public class CameraChase : MonoBehaviour
     public float YMax;
     public float YMin;
 
+    float cameraXMin, cameraXMax, cameraYMin, cameraYMax;
+
     // 플레이어와 커서 사이 어느정도 위치에 카메라가 놓일지
     private float Ratio;
 
     // Start is called before the first frame update
     void Start()
     {
-        XMax = 17.8f;
-        XMin = 0.0f;
-        YMax = 4.45f;
-        YMin = -2.5f;
+        SetLimit();
+    }
+
+    void SetLimit()
+    {
+        Camera cam = Camera.main;
+
+        float height = 2f * cam.orthographicSize;
+        float width = height * cam.aspect;
+
+        cameraXMin = XMin + width / 2;
+        cameraXMax = XMax - width / 2;
+
+        cameraYMin = YMin + height / 2;
+        cameraYMax = YMax - height / 2;
     }
 
     // Update is called once per frame
@@ -30,7 +43,7 @@ public class CameraChase : MonoBehaviour
         Vector3 Cursor = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 Diff = Cursor - transform.position;
         Ratio = Mathf.Max( Mathf.Sqrt(Diff.x * Diff.x + Diff.y * Diff.y), MinRatio);
-        transform.position = new Vector3(Mathf.Clamp((player.position.x* Ratio + Cursor.x)/(Ratio+1.0f),XMin, XMax),
-            Mathf.Clamp((player.position.y* Ratio + Cursor.y)/(Ratio + 1.0f), YMin, YMax), -10);
+        transform.position = new Vector3(Mathf.Clamp((player.position.x* Ratio + Cursor.x)/(Ratio+1.0f), cameraXMin, cameraXMax),
+            Mathf.Clamp((player.position.y* Ratio + Cursor.y)/(Ratio + 1.0f), cameraYMin, cameraYMax), -10);
     }
 }
--- mini_game/Assets/Scripts/MainLogic.cs
+++ mini_game/Assets/Scripts/MainLogic.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class MainLogic : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] points;
+
+    public GameObject monsterObject;
+
+    public float createTime;
+
+    public int maxMonster = 100;
+
+    public bool isGameOver = false;
+
     void Start()
     {
-        
+        points = GameObject.Find("SpawnPoint").GetComponentsInChildren<Transform>();
+        print(points.Length);
+        if (points.Length > 0)
+        {
+            StartCoroutine(this.CreateMonster());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator CreateMonster()
     {
+        while (!isGameOver)
+        {
+            int monsterCount = (int)GameObject.FindGameObjectsWithTag("Enemy").Length;
+
+            if (monsterCount < maxMonster)
+            {
+                yield return new WaitForSeconds(createTime);
+                
+                int idx = Random.Range(1, points.Length);
+                Vector3 random = new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), 0);
+                Instantiate(monsterObject, points[idx].position + random, points[idx].rotation);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
     }
 }
